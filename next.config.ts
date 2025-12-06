b1e44d48cf5c72a880f1ce97bc81055f5f2212a0
Unique Neupane <khemrajneupane111@gmail.com>
2025-12-06 11:44:29 +0545

    fix errro

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: [
-      "pixabay.com",
-      "cdn.pixabay.com",
-      "images.unsplash.com",
-      "i.imgur.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
     ],
   },
 };
